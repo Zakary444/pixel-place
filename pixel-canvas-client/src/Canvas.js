7765f6dcc1f1859fef995d6e131844fde83d9d9a
Zakary444 <131569016+Zakary444@users.noreply.github.com>
2023-05-24 02:36:26 -0500

    new

--- pixel-canvas-client/src/Canvas.js
+++ pixel-canvas-client/src/Canvas.js
@@ -4,80 +4,85 @@ import io from 'socket.io-client';
 import { VariableSizeGrid as Grid } from 'react-window';
 
 const Pixel = ({ columnIndex, rowIndex, style, color, pixels, setPixels }) => {
-    const handlePixelClick = (x, y) => {
-        axios.post('http://45.33.114.158:3001/api/canvas', { x, y, color })
-            .then(response => {
-                setPixels(prevPixels => {
-                    const newPixels = [...prevPixels];
-                    newPixels[y * 120 + x] = color;
-                    return newPixels;
-                });
-            })
-            .catch(console.error);
-    };
+  const handlePixelClick = (x, y) => {
+    axios
+      .post('http://45.33.114.158:3001/api/canvas', { x, y, color })
+      .then(response => {
+        setPixels(prevPixels => {
+          const newPixels = [...prevPixels];
+          newPixels[y * 120 + x] = color;
+          return newPixels;
+        });
+      })
+      .catch(console.error);
+  };
 
-    return ( <
-        div className = "pixel"
-        style = {
-            {
-                ...style,
-                backgroundColor: pixels[rowIndex * 120 + columnIndex],
-            }
-        }
-        onClick = {
-            () => handlePixelClick(columnIndex, rowIndex)
-        }
-        />
-    );
+  return (
+    <div
+      className="pixel"
+      style={{
+        ...style,
+        backgroundColor: pixels[rowIndex * 120 + columnIndex],
+      }}
+      onClick={() => handlePixelClick(columnIndex, rowIndex)}
+    />
+  );
 };
 
 const Canvas = ({ color }) => {
-    const [pixels, setPixels] = useState(new Array(120 * 120).fill('#FFFFFF'));
-    const socket = io('http://45.33.114.158:3001');
+  const [pixels, setPixels] = useState(new Array(120 * 120).fill('#FFFFFF'));
+  const socket = io('http://45.33.114.158:3001');
 
-    useEffect(() => {
-        axios.get('http://45.33.114.158:3001/api/canvas')
-            .then(response => {
-                const pixels = new Array(120 * 120).fill('#FFFFFF');
-                response.data.forEach(pixel => {
-                    pixels[pixel.y * 120 + pixel.x] = pixel.color;
-                });
-                setPixels(pixels);
-            })
-            .catch(console.error);
-
-        socket.on('pixelUpdated', (pixel) => {
-            setPixels(prevPixels => {
-                const newPixels = [...prevPixels];
-                newPixels[pixel.y * 120 + pixel.x] = pixel.color;
-                return newPixels;
-            });
+  useEffect(() => {
+    // Retrieve initial state from the server
+    axios
+      .get('http://45.33.114.158:3001/api/canvas')
+      .then(response => {
+        const initialPixels = new Array(120 * 120).fill('#FFFFFF');
+        response.data.forEach(pixel => {
+          initialPixels[pixel.y * 120 + pixel.x] = pixel.color;
         });
+        setPixels(initialPixels);
+      })
+      .catch(console.error);
 
-        return () => socket.off('pixelUpdated');
-    }, []);
+    socket.on('initialState', receivedPixels => {
+      setPixels(receivedPixels);
+    });
 
-    return ( < Grid className = 'Canvas'
-    columnCount={120}
-    columnWidth={index => 10}  // function to calculate column width
-    rowCount={120}
-    rowHeight={index => 10}  // function to calculate row height
-    height={1200}
-    width={1200}
-  >
-    {({ columnIndex, rowIndex, style }) => (
-      <Pixel
-        columnIndex={columnIndex}
-        rowIndex={rowIndex}
-        style={style}
-        color={color}
-        pixels={pixels}
-        setPixels={setPixels}
-      />
-    )}
-  </Grid>
+    socket.on('pixelUpdated', pixel => {
+      setPixels(prevPixels => {
+        const newPixels = [...prevPixels];
+        newPixels[pixel.y * 120 + pixel.x] = pixel.color;
+        return newPixels;
+      });
+    });
 
-    );
+    return () => socket.off('pixelUpdated');
+  }, []);
+
+  return (
+    <Grid
+      className="Canvas"
+      columnCount={120}
+      columnWidth={() => 10} // function to calculate column width
+      rowCount={120}
+      rowHeight={() => 10} // function to calculate row height
+      height={1200}
+      width={1200}
+    >
+      {({ columnIndex, rowIndex, style }) => (
+        <Pixel
+          columnIndex={columnIndex}
+          rowIndex={rowIndex}
+          style={style}
+          color={color}
+          pixels={pixels}
+          setPixels={setPixels}
+        />
+      )}
+    </Grid>
+  );
 };
 
 export default Canvas;
